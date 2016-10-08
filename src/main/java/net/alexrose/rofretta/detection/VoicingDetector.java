cd781da62de7fd831e8a261664958aea570bfc7b
Alex Rose <alex@alexrose.net>
2016-10-08 09:03:40 -0700

    Check in a rough draft of what a detection interface might look like

---
+++ src/main/java/net/alexrose/rofretta/detection/VoicingDetector.java
@@ -0,0 +1,30 @@
+package net.alexrose.rofretta.detection;
+
+import net.alexrose.rofretta.core.Instrument;
+import net.alexrose.rofretta.core.InstrumentString;
+import net.alexrose.rofretta.core.Voicing;
+
+import java.util.List;
+
+/**
+ * You plucked something on the guitar that sounded good.
+ * You saw your idol play something.
+ * Your theory isn't hot or you're a bit lazy - what WAS that, probably?
+ * <p>
+ * The VoicingDetector will attempt to figure out it out for you.
+ */
+public class VoicingDetector {
+
+    /**
+     * Given
+     *
+     * @return
+     */
+    public List<Voicing> detect(List<InstrumentString> strings, Instrument tuning) {
+
+    }
+
+    public List<Voicing> detect(String input) {
+
+    }
+}
